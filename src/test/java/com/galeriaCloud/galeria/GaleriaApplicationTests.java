2808abffe8fc84c0a5d94fe86fd312ef486faf64
César Viana Barros <126491073+CVbarro@users.noreply.github.com>
2025-05-27 20:01:45 -0300

    Apis finalizadas

---
+++ src/test/java/com/galeriaCloud/galeria/GaleriaApplicationTests.java
@@ -0,0 +1,10 @@
+package com.galeriaCloud.galeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaleriaApplicationTests {
+
+
+}
